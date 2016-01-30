d4edddcb60c1b4aa186c62001d3c1de5c55da60f
Michael DaSilva <programmedmikey@gmail.com>
2016-01-30 08:58:37 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var PORT = process.env.PORT || 3000; 
+
+app.use(express.static(__dirname + '/public')); 
+
+app.listen(PORT, function () { 
+	console.log('Express server started on port ' + PORT '!'); 
+
+});
